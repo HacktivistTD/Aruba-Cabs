d20c16cd1f8fcf22d92664aed56caefed16f67da
HacktivistTD <thusharadilrukshatd@gmail.com>
2025-08-09 22:44:17 +0530

    remove gfont

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,8 @@
 import './globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import { Parkinsans } from 'next/font/google';
 
-// Load the font from Google Fonts
-const parkinsans = Parkinsans({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'], // you can add more weights if needed
-  display: 'swap',
-});
+
 
 export const metadata = {
   title: 'Aruba Cab Services',
@@ -18,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={parkinsans.className}>
+    <html lang="en">
       <body className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow container mx-auto p-6">{children}</main>
